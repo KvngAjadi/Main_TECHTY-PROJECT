47dd8848f6718b7eed287b09697f1178e3450ce5
KvngAjadi <ajadiachiever@gmail.com>
2025-08-18 13:29:40 +0100

    worked on the functioning of the empowerment component and also some other part of my work

--- src/components/EmpowerComp.jsx
+++ src/components/EmpowerComp.jsx
@@ -1,77 +1,211 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 const EmpowerComp = () => {
+  const [first, setFirst] = useState(true);
+  const [second, setSecond] = useState(false);
+  const [third, setThird] = useState(false);
+
   return (
-    <div>
-      <div className="w-full flex items-center justify-center mt-25">
-        <h1 className="text-black font-space font-bold text-4xl px-15">
-          Empower your business <br />
-          with cutting-edge <span className="text-[#28D08A]">solutions.</span>
-        </h1>
+    <section className="flex flex-col font-space w-full">
+      <div className="flex items-center justify-center py-25">
+        <p className="text-center text-5xl font-extrabold">
+          Empower your business
+          <br /> with cutting-edge{" "}
+          <span className="text-green-500">solutions</span>
+        </p>
       </div>
-      <div className="flex mt-20">
-        <div className="mt-10 pl-10 hover:border-">
-          <h1 className="text-[#28D08A] text-6xl font-space font-bold">01</h1>
+      <div className="flex mx-15 border-b-1 border-b-gray-300 mb-10 gap-2">
+        <div
+          onClick={() => {
+            setFirst(true), setSecond(false), setThird(false);
+          }}
+          className={
+            first
+              ? "flex items-center gap-3 pb-7 cursor-pointer border-b-1 border-green-500"
+              : "flex items-center gap-3 pb-7 cursor-pointer"
+          }
+        >
+          <h1 className={`text-6xl font-extrabold ${first ? "text-green-500" : "text-black"}`}>01</h1>
+          <div className="flex flex-col gap-2 ">
+            <p className="font-semibold text-lg">Collect Ideas</p>
+            <p>Creating or improving products to meet our user needs.</p>
+          </div>
         </div>
-        <div className="flex flex-col font-space mt-10 pl-3 ">
-          <p className="text-[20px] font-semibold">Collect Ideas</p>
-          <p>
-            Creating or improving products to meet <br />
-            our users needs
-          </p>
+        <div
+          onClick={() => {
+            setFirst(false), setSecond(true), setThird(false);
+          }}
+          className={
+            second
+              ? "flex items-center gap-3 pb-7 cursor-pointer border-b-1 border-green-500"
+              : "flex items-center gap-3 pb-7 cursor-pointer"
+          }
+        >
+          <h1 className={`font-extrabold text-6xl ${second ? "text-green-500" : "text-black"} `}>02</h1>
+          <div className="flex flex-col gap-2">
+            <p className="font-semibold text-lg">Data Analysis</p>
+            <p>Creating or improving products to meet our user needs.</p>
+          </div>
         </div>
-        <div className="mt-10 pl-10">
-          <h1 className="text-[#28D08A] text-6xl font-space font-bold">02</h1>
-        </div>
-        <div className="flex flex-col font-space mt-10 pl-3 ">
-          <p className="text-[20px] font-semibold">Collect Ideas</p>
-          <p>
-            Creating or improving products to meet <br />
-            our users needs
-          </p>
-        </div>
-        <div className="mt-10 pl-10">
-          <h1 className="text-[#28D08A] text-6xl font-space font-bold">03</h1>
-        </div>
-        <div className="flex flex-col font-space mt-10 pl-3 ">
-          <p className="text-[20px] font-semibold">Collect Ideas</p>
-          <p>
-            Creating or improving products to meet <br />
-            our users needs
-          </p>
-        </div>
-      </div>
-      <center>
-        <hr className="mt-5" />
-      </center>
-      <div className="flex mt-20 gap-10 px-15">
-        <img src="/image 2.png" alt="" className="rounded-lg" />
-        <div className="flex flex-col font-space gap-5 mt-10">
-          <p className="text-2xl font-semibold">Collect Ideas</p>
-          <p className="text-[15px] font-normal">
-            With our Techty company, you can create landing pages that convert
-            more <br /> visitors than any other website. You can easily create a
-            page using a variety <br /> of unique blocks.
-          </p>
-          <p className="text-[20px] font-normal">
-            We collect ideas from different design inspirations,
-          </p>
-          <p className="text-[20px] font-normal">
-            Analysis data for any kind of corrections.
-          </p>
-          <p className="text-[20px] font-normal">
-            Finalize the product for the production to be done.
-          </p>
-          <Button
-            name="Request a Free Demo"
-            style={
-              "bg-black hover:bg-[#28D08A] text-white text-[15px] items-center flex justify-center rounded-md w-60 font-space h-13 font-semibold"
-            }
-          />
+        <div
+          onClick={() => {
+            setFirst(false), setSecond(false), setThird(true);
+          }}
+          className={
+            third
+              ? "flex items-center gap-3 pb-7 cursor-pointer border-b-1 border-green-500"
+              : "flex items-center gap-3 pb-7 cursor-pointer"
+          }
+        >
+          <h1 className={`font-extrabold text-6xl ${third ? "text-green-500" : "text-black"}`}>03</h1>
+          <div className="flex flex-col gap-2">
+            <p className="font-semibold text-lg">Analyze Products</p>
+            <p>Creating or improving products to meet our user needs.</p>
+          </div>
         </div>
       </div>
-    </div>
+      <div>
+        {first ? (
+          <div className="flex items-center  mx-15 gap-10">
+            <img
+              src="/image 2.png"
+              alt="image of a phone and laptop"
+              className="w-[550px] rounded-lg"
+            />
+            <div className="flex flex-col gap-7">
+              <h1 className="text-3xl font-semibold">Collect Ideas</h1>
+              <p className="text-md text-[#4E4E4E] text-justify">
+                With our Techty company, you can create landing pages that
+                convert more <br /> visitors than any other website. You can
+                easily create a page using a variety <br /> of unique blocks.
+              </p>
+              <div className="flex flex-col gap-3 ">
+                <div className="flex items-center gap-2">
+                  <img src="/" />
+                  <p className="text-xl">
+                    We collect ideas from different design inspirations,
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Analysis data for any kind of corrections.
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Finalize the product for the production to be done.
+                  </p>
+                </div>
+              </div>
+              <div className="flex">
+                <Button
+                  name="Request a Free Demo"
+                  style={"bg-black text-white px-5 hover:bg-green-500"}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {second ? (
+          <div className="flex items-center  mx-15 mb-10 gap-10">
+            <img
+              src="/collect o.png"
+              alt="image of pencil, paper and book"
+              className="w-[550px]"
+            />
+            <div className="flex flex-col gap-7">
+              <h1 className="text-3xl font-semibold">Data Analysis</h1>
+              <p className="text-md text-[#4E4E4E] text-justify">
+                With our Techty company, you can create landing pages that
+                convert more <br /> visitors than any other website. You can
+                easily create a page using a variety <br /> of unique blocks.
+              </p>
+              <div className="flex flex-col gap-3 ">
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    We collect ideas from different design inspirations,
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Analysis data for any kind of corrections.
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Finalize the product for the production to be done.
+                  </p>
+                </div>
+              </div>
+              <div className="flex">
+                <Button
+                  name="Request a Free Demo"
+                  style={"bg-black text-white px-5 hover:bg-green-500"}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {third ? (
+          <div className="flex items-center  mx-15 mb-10 gap-10">
+            <img
+              src="/collect 1.png"
+              alt="image of 8 colored papers"
+              className="w-[550px]"
+            />
+            <div className="flex flex-col gap-7">
+              <h1 className="text-3xl font-semibold">Finalize Product</h1>
+              <p className="text-md text-[#4E4E4E] text-justify">
+                With our Techty company, you can create landing pages that
+                convert more <br /> visitors than any other website. You can
+                easily create a page using a variety <br /> of unique blocks.
+              </p>
+              <div className="flex flex-col gap-3 ">
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    We collect ideas from different design inspirations,
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Analysis data for any kind of corrections.
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Finalize the product for the production to be done.
+                  </p>
+                </div>
+              </div>
+              <div className="flex">
+                <Button
+                  name="Request a Free Demo"
+                  style={"bg-black text-white px-5 hover:bg-green-500"}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+
+    </section>
   );
 };
 
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -20,31 +20,35 @@ const Service = () => {
             <Button name='Request a Free Demo' style={"bg-white text-black rounded-md border-1 border-black font-semibold"}/>
         </div>
         </nav>
-        <div className='mt-20'>
+        <div className='bg-[#F0F8FF]'>
           <div className='text-center font-space'>
-          <h1 className='text-5xl font-bold'>We provide the best <span className='text-[#28D08A] text-5xl font-bold'>service</span> solutions.</h1>
+          <h1 className='text-5xl font-bold '>We provide the best <span className='text-[#28D08A] text-5xl font-bold'>service</span> solutions.</h1>
           <p className='font-space mt-5 text-[20px] text-gray-600'>We're constantly pushing the boundaries of what's possible and seeking new ways to <br /> improve our services and help our clients achieve their goals.</p>
           </div>
-          <div className='flex justify-between p-4'>
-          <div className='bg-gray-700 w-70 h-80 p-5 mt-5 rounded-md'>
-            <img src="/icon 1.png" alt="" className='mt-3' />
+          <div className='flex justify-between p-10'>
+          <div className='bg-gray-700 w-70 h-auto p-5 mt-5 rounded-md flex flex-col'>
+            <img src="/icon 1.png" alt="" className='mt-3 h-23 w-23' />
             <h1 className='font-bold mt-3 font-space text-2xl'>Online Marketing</h1>
-            <p className='font-space mt-3 text-[20px]'>In the new era of technology <br />we look in the future with <br />certainty and pride.</p>
+            <p className='font-space mt-3 text-[17px]'>In the new era of technology <br />we look in the future with <br />certainty and pride.</p>
+            <img src="/Vector.png" alt="" className='w-7 h-7 mt-3'/>
           </div>
-          <div className='bg-gray-700 w-70 h-80 p-3 mt-5 rounded-md'>
-            <img src="/icon 1.png" alt="" className='mt-3' />
+          <div className='bg-gray-700 w-70 h-auto p-5 mt-5 rounded-md flex flex-col'>
+            <img src="/icon 1.png" alt="" className='mt-3 h-23 w-23' />
             <h1 className='font-bold mt-3 font-space text-2xl'>Online Marketing</h1>
-            <p className='font-space mt-3'>In the new era of technology <br />we look in the future with <br />certainty and pride.</p>
+            <p className='font-space mt-3 text-[17px]'>In the new era of technology <br />we look in the future with <br />certainty and pride.</p>
+            <img src="/Vector.png" alt="" className='w-7 h-7 mt-3'/>
           </div>
-          <div className='bg-gray-700 w-70 h-80 p-3 mt-5 rounded-md'>
-            <img src="/icon 1.png" alt="" className='mt-3' />
+          <div className='bg-gray-700 w-70 h-auto p-5 mt-5 rounded-md flex flex-col'>
+            <img src="/icon 1.png" alt="" className='mt-3 h-23 w-23' />
             <h1 className='font-bold mt-3 font-space text-2xl'>Online Marketing</h1>
-            <p className='font-space mt-3'>In the new era of technology <br />we look in the future with <br />certainty and pride.</p>
+            <p className='font-space mt-3 text-[17px]'>In the new era of technology <br />we look in the future with <br />certainty and pride.</p>
+            <img src="/Vector.png" alt="" className='w-7 h-7 mt-3'/>
           </div>
-          <div className='bg-gray-700 w-70 h-80 p-3 mt-5 rounded-md'>
-            <img src="/icon 1.png" alt="" className='mt-3' />
-            <h1 className='font-bold font-space text-2xl mt-5'>Online Marketing</h1>
-            <p className='font-space mt-5'>In the new era of technology <br />we look in the future with <br />certainty and pride.</p>
+          <div className='bg-gray-700 w-70 h-auto p-5 mt-5 rounded-md flex flex-col'>
+            <img src="/icon 1.png" alt="" className='mt-3 h-23 w-23' />
+            <h1 className='font-bold mt-3 font-space text-2xl'>Online Marketing</h1>
+            <p className='font-space mt-3 text-[17px]'>In the new era of technology <br />we look in the future with <br />certainty and pride.</p>
+            <img src="/Vector.png" alt="" className='w-7 h-7 mt-3'/>
           </div>
           </div>
         </div>
